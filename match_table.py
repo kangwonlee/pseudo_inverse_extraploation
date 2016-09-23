0505c2119e323b66765e1efcb91d2bc243cf32a4
KangWon Lee <kangwon_fwd@naver.com>
2016-09-23 17:08:05 +0900

    started match_table.py : read req and txt files

---
+++ match_table.py
@@ -0,0 +1,18 @@
+from table_to_csv import read_txt_lines
+
+
+def main(req_filename, table_filename):
+    req_lines = read_txt_lines(req_filename)
+    table_lines = read_txt_lines(table_filename, 'utf8')
+
+    for req_line in req_lines:
+        print(req_line)
+
+    for table_line in table_lines:
+        print(table_line)
+
+
+if __name__ == '__main__':
+    import sys
+    if 3 == len(sys.argv):
+        main(sys.argv[1], sys.argv[2])
