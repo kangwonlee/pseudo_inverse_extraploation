df706af2008b19b35c98bbf74d1639726b609743
KangWon Lee <kangwon_fwd@naver.com>
2016-09-23 23:18:17 +0900

    read match table file and print

--- pt_to_req.py
+++ pt_to_req.py
@@ -1,11 +1,17 @@
 # -*- coding: utf8 -*-
 
 from table_to_csv import read_txt_lines
+from match_table import str_list_to_string
 
 
 def main(match_filename):
-    pass
+    lines = read_txt_lines(match_filename, 'utf8')
+    print(str_list_to_string(lines))
 
 
 if __name__ == '__main__':
-    main()
+
+    import sys
+
+    if 2 == len(sys.argv):
+        main(sys.argv[1])
