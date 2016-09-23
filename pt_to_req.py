e5de5065791851e1163c087cbee69ae0715028fa
KangWon Lee <kangwon_fwd@naver.com>
2016-09-24 07:34:17 +0900

    added match_table_to_dict() : using regular expression, find key information

--- pt_to_req.py
+++ pt_to_req.py
@@ -1,18 +1,34 @@
 # -*- coding: utf8 -*-
+import re
 
-from table_to_csv import read_txt_lines
 from match_table import str_list_to_string
+from table_to_csv import read_txt_lines
 
 TAB = chr(9)
 
 
 def main(match_filename):
-    # read file
     lines = read_txt_lines(match_filename, 'utf8')
     tab_separated_lines = tab_separate(lines)
 
     for tab_sep_line in tab_separated_lines:
         print(str_list_to_string(tab_sep_line))
+
+    match_table_to_dict(tab_separated_lines)
+
+
+def match_table_to_dict(tab_separated_lines):
+    result = {}
+
+    point_search = re.compile(r'(.+)\s\((.+)\)\s=\s(.+)')
+
+    for tab_sep_line in tab_separated_lines:
+        key = tab_sep_line[0]
+        values = tab_sep_line[1:]
+        result[key] = []
+
+        for value in values:
+            print(point_search.findall(value))
 
 
 def tab_separate(lines_list):
