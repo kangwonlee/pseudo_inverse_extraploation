0ce19aef0d603014db902508a44a0fdc2fdb78f8
KangWon Lee <kangwon_fwd@naver.com>
2016-09-23 23:30:12 +0900

    read match table file and split by tab

--- pt_to_req.py
+++ pt_to_req.py
@@ -3,10 +3,24 @@
 from table_to_csv import read_txt_lines
 from match_table import str_list_to_string
 
+TAB = chr(9)
+
 
 def main(match_filename):
+    # read file
     lines = read_txt_lines(match_filename, 'utf8')
-    print(str_list_to_string(lines))
+    tab_separated_lines = tab_separate(lines)
+
+    for tab_sep_line in tab_separated_lines:
+        print(str_list_to_string(tab_sep_line))
+
+
+def tab_separate(lines_list):
+    result = []
+    for line in lines_list:
+        result.append(line.strip().split(TAB))
+
+    return result
 
 
 if __name__ == '__main__':
