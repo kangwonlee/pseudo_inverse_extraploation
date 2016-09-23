5d92db4ea24b1585a9f8ab6aa3a79790b58f3331
KangWon Lee <kangwon_fwd@naver.com>
2016-09-23 17:39:58 +0900

    started get_req_table() : convert req file lines to dictionary

--- match_table.py
+++ match_table.py
@@ -1,15 +1,21 @@
-# -*- coding: cp949 -*-
+# -*- coding: utf8 -*-
 from table_to_csv import read_txt_lines
 
 
 def main(req_filename, table_filename):
     req_lines, table_lines = read_txt_lines(req_filename), read_txt_lines(table_filename, 'utf8')
 
-    for req_line in req_lines:
-        print(req_line)
+    get_req_table(req_lines)
 
     for table_line in table_lines:
         print(table_line)
+
+
+def get_req_table(req_lines):
+    result = {}
+    for req_line in req_lines:
+        req_row_list = get_row_list(req_line)
+        print(req_row_list)
 
 
 def get_row_list(req_line, sep='\t'):
