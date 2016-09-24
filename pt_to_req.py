bd62fa47f8b958f1250919eb2b29f3aefa810488
KangWon Lee <kangwon_fwd@naver.com>
2016-09-24 09:17:16 +0900

    moved join_comma_space_re_group_map_helper into match_table_to_dict() match_table_to_dict() : iterate through found items from mapping of regular expression

--- pt_to_req.py
+++ pt_to_req.py
@@ -17,25 +17,26 @@ def main(match_filename):
     match_table_to_dict(tab_separated_lines)
 
 
-def join_comma_space_re_group_map_helper(items):
-    return ', '.join(items[0])
-
-
 def match_table_to_dict(tab_separated_lines):
+    def join_comma_space_re_group_map_helper(items):
+        return ', '.join(items[0])
+
+    def found_to_dict(found):
+        return {'name': found[0], 'number': found[1], 'point': found[2]}
+
     result = {}
 
     point_search = re.compile(r'(.+)\s\((.+)\)\s=\s(.+)')
 
     for tab_sep_line in tab_separated_lines:
-        key = tab_sep_line[0]
-        values = tab_sep_line[1:]
-        result[key] = []
+        req_key = tab_sep_line[0]
+        pt_string_list = tab_sep_line[1:]
+        pt_list_list_tuple = map(point_search.findall, pt_string_list)
 
-        row_list = map(point_search.findall, values)
-        cell_string_list = map(join_comma_space_re_group_map_helper, row_list)
-
-        row_string = ', '.join(cell_string_list)
-        print(row_string)
+        for pt_list_tuple in pt_list_list_tuple:
+            result[pt_list_tuple[0][1]] = {'req_key': req_key,
+                                           'name': pt_list_tuple[0][0],
+                                           'point': pt_list_tuple[0][2]}
 
 
 def tab_separate(lines_list):
