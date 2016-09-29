30826543e7e5c6efdf121a8337b9c9a602d9f0df
KangWon Lee <kangwon_fwd@naver.com>
2016-09-30 01:09:30 +0900

    indicated encoding

--- test/test_my_string.py
+++ test/test_my_string.py
@@ -6,7 +6,7 @@ import my_string
 
 class TestMyString(unittest.TestCase):
     def test_repr(self):
-        s = my_string.MyStr('가나다')
+        s = my_string.MyStr('가나다', encoding='utf8')
         result = repr(s)
         self.assertEqual("'가나다'", result)
 
